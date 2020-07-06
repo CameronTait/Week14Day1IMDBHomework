bfaeadc4bb1ac01f2ea252621a3fce34164e7534
CameronTait <cameron-tait@hotmail.com>
2020-07-06 20:37:10 +0100

    Have movie list rendering on screen.  All that is left is to turn to links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import MovieBox from "./containers/MovieBox";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <MovieBox />;
 }
 
 export default App;
